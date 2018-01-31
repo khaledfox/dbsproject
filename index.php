2da75077894bc5e99d00cdcf65f560e0cc6f4381
khaledfox <36004154+khaledfox@users.noreply.github.com>
2018-01-31 15:59:07 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,5 @@ echo "helow world form heroku server<br/>";
 echo "this file is stored on github"
 ?>
 
-<p> this is <br/>
-  normal<br/>
-  html<br/>
-  <image src="https://students.dbs.ie/site_image/dbs_logo.png"
-         
-         <?php echo "and this is from php again." ?>
-         
+
+  <image src="https://students.dbs.ie/site_image/dbs_logo.png"      
